c6338697df607e09c905ef1ba2a3bea5a2622ab4
samirfaritov <samirfaritov6@gmail.com>
2023-10-30 15:28:24 +0500

    JS DOM 1

---
+++ main.js
@@ -0,0 +1,45 @@
+// nomi orqali olib kelish 
+// let h1 = document.getElementsByTagName('h1')
+// console.log(h1);
+
+// class orqali olib kelish
+// let h1class = document.getElementsByClassName('genrix')
+// console.log(h1class);
+
+// Id orqali olib kelish
+// let h1Id = document.getElementById('salom')
+// console.log(h1Id);
+
+
+
+// Id orqali olib kelish
+// let a = document.querySelectorAll('#salom')
+// console.log(a);
+
+// class orqali olib kelish
+// let b = document.querySelectorAll('.genrix')
+// console.log(b);
+
+// nomi olib kelish
+// let c = document.querySelector('h1')
+// console.log(c);
+
+// hammasiga tasir qiladi
+// let f = document.querySelectorAll('h1')
+// console.log(f);
+
+
+// let a = document.querySelector('.txt')
+// let box = document.querySelector('.box')
+
+// nomini almashtiradi
+// a.innerText = 'Hello'
+
+
+// a.innerHTML = '<span>hi</span>'
+
+
+// style bersa bo'ladi
+// box.computedStyleMap.width = '200px'
+// box.computedStyleMap.height = '200px'
+// box.computedStyleMap.backgroundColor = 'red'
